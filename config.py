b47c0cd4798be7b8898c144cf17bac2adb1a5320
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2022-03-01 09:45:39 +0200

    added venv in gitignore

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-
-GOOGLE_CLIENT_ID = "374247044671-v7ufr687dci228kn08rtcvrbm1ql8fod.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
+from dotenv import load_dotenv
+load_dotenv()
+import os
+GOOGLE_CLIENT_ID =os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET =os.getenv("GOOGLE_CLIENT_SECRET")
